1ededa3d4f8009503b6cbc716e9267a5509a17ec
CryptoShree <101166268+CryptoShree@users.noreply.github.com>
2022-06-06 15:01:12 -0700

    update words

--- js/packages/web/src/views/home/components/SalesList/index.tsx
+++ js/packages/web/src/views/home/components/SalesList/index.tsx
@@ -44,8 +44,8 @@ export const SalesListView = (props: { collectionMintFilter?: string }) => {
       {!props.collectionMintFilter && (
         <Banner
           src="/main-banner.svg"
-          headingText="The amazing world of Metaplex."
-          subHeadingText="Buy exclusive Metaplex NFTs."
+          headingText="90210crypto NFT Marketplace."
+          subHeadingText="Buy exclusive 90210 NFTs."
           actionComponent={<HowToBuyModal buttonClassName="secondary-btn" />}
           useBannerBg
         />
